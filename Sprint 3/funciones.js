834e3981e4a3085c6802d0e6e1b73c378a701be9
DanielDMolina <danieldominguezmolina@hotmail.com>
2021-09-11 17:52:29 -0500

    imc final final

--- Sprint 3/funciones.js
+++ Sprint 3/funciones.js
@@ -1,5 +1,6 @@
 function imc(){
-    var x =parseInt(document.getElementById('valor1').value);
-    var y =parseInt(document.getElementById('valor2').value);
-document.getElementById('el-resultado').innerHTML = x+y;
+    var x =parseFloat(document.getElementById('valor1').value);
+    var y =parseFloat(document.getElementById('valor2').value);
+    document.getElementById('el-resultado').innerHTML = (y/Math.pow(x,2)).toFixed(2);
+    
 }
