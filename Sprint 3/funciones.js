6b7759dc6c08570c4b1bb03ef782f065c5fda418
DanielDMolina <danieldominguezmolina@hotmail.com>
2021-09-11 16:41:33 -0500

    imc

---
+++ Sprint 3/funciones.js
@@ -0,0 +1,5 @@
+function imc(){
+    var x =parseInt(document.getElementById('valor1').value);
+    var y =parseInt(document.getElementById('valor2').value);
+document.getElementById('el-resultado').innerHTML = x+y;
+}
