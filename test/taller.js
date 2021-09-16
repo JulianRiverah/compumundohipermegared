b9170e5def6ec5668993c787deeb554d6558f484
Julian Rivera <julian.riverah@hotmail.com>
2021-09-16 07:07:28 -0500

    Update taller.js

--- test/taller.js
+++ test/taller.js
@@ -1,6 +1,18 @@
-var numero = prompt("Digite numero");
-if(numero>0){
-    alert("Numero mayor a cero");
-}else{
-    alert("Numero menor a cero")
-}
+// var numero = prompt("Digite numero");
+// if(numero>0){
+//     alert("Numero mayor a cero");
+// }else{
+//     alert("Numero menor a cero")
+// }
+
+var n1 = parseInt(prompt("Digite numero1"));
+var n2 = parseInt(prompt("Digite numero2"));
+
+sum = n1 + n2;
+res = n1 - n2;
+div = n1 / n2;
+mul = n1 * n2;
+pot = n1**2;
+
+
+alert("Suma: "+ sum + "\n" + "Resta: " + res + "Division: " + div + "Multiplicacion: " + mul)
