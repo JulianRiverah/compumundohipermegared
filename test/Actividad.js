257851824bbdb01f9fe7745ab084c28123d3c681
lesliesalas <89642043+lesliesalas@users.noreply.github.com>
2021-09-16 06:17:40 -0500

    create actividad.js

---
+++ test/Actividad.js
@@ -0,0 +1,10 @@
+function cambio_funcion(){
+    var elemento = document.getElementById("uno");
+    
+    if (elemento.style.display=="block"){
+        elemento.style.display="none";
+    }else{
+        elemento.style.display="block";
+    }
+
+}
