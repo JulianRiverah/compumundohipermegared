183f5dcaf1ae2ae5beabdd53ad3f438f6ad8e79a
Julian Rivera <julian.riverah@hotmail.com>
2021-09-16 19:40:25 -0500

    taller

--- test/taller.js
+++ test/taller.js
@@ -1,33 +1,27 @@
+//3333333333
 // var numero = prompt("Digite numero");
 // if(numero>0){
 //     alert("Numero mayor a cero");
 // }else{
 //     alert("Numero menor a cero")
 // }
-
+//444444444444
 // var n1 = parseInt(prompt("Digite numero1"));
 // var n2 = parseInt(prompt("Digite numero2"));
-
 // sum = n1 + n2;
 // res = n1 - n2;
 // div = n1 / n2;
 // mul = n1 * n2;
-// pot = n1**2;
-
-
 // alert("Suma: "+ sum + "\n" + "Resta: " + res + "Division: " + div + "Multiplicacion: " + mul)
-
+//555555555555
 // var fecha = prompt("ingrese su fecha de nacimiento","dd/mm/aaaa");
-
 // var values = fecha.split("/");
 // var dia = values[0];
 // var mes = values[1];
 // var ano = values[2];
-
 // // alert(ano);
 // // alert(mes);
 // // alert(dia);
-
 // // Fecha actual
 // var fecha_hoy = new Date();
 // var ahora_ano = fecha_hoy.getFullYear();
@@ -41,22 +35,28 @@
 // // alert(ahora_ano);
 // // alert(ahora_mes);
 // // alert(ahora_dia);
-//********************************
+//6666666666666666
 
-var fecha = prompt("ingrese su fecha de nacimiento","dd/mm/aaaa");
-var values = fecha.split("/");
-var dia = values[0];
-var mes = values[1];
-var ano = values[2];
-var fecha_hoy = new Date();
-var ahora_ano = fecha_hoy.getFullYear();
-var ahora_mes = fecha_hoy.getMonth();
-var ahora_dia = fecha_hoy.getDay();
-edad = ahora_ano-ano;
-if(edad<18){
-    alert("Es menor de edad ("+edad+" años)");
-}else if (edad>60){
-    alert("Es adulto mayor ("+edad+")");
-}else{
-    alert("Es mayor de edad("+edad+")");
-}
+// var fecha = prompt("ingrese su fecha de nacimiento","dd/mm/aaaa");
+// var values = fecha.split("/");
+// var dia = values[0];
+// var mes = values[1];
+// var ano = values[2];
+// var fecha_hoy = new Date();
+// var ahora_ano = fecha_hoy.getFullYear();
+// var ahora_mes = fecha_hoy.getMonth();
+// var ahora_dia = fecha_hoy.getDay();
+// edad = ahora_ano-ano;
+// if(edad<18){
+//     alert("Es menor de edad ("+edad+" años)");
+// }else if (edad>60){
+//     alert("Es adulto mayor ("+edad+")");
+// }else{
+//     alert("Es mayor de edad("+edad+")");
+// }
+
+//777777777777777
+// var nombre = prompt("Ingrese su nombre");
+
+// nombre=nombre.split(" ").join("");
+// alert(nombre.length);
