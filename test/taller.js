d31f6f851742b590f91b8f22202d6dbe88bad9e9
Julian Rivera <julian.riverah@hotmail.com>
2021-09-16 20:13:34 -0500

    taller

--- test/taller.js
+++ test/taller.js
@@ -1,11 +1,11 @@
-//3333333333
+//3.	Recibir un número entero y mostrar en consola si el número es mayor que cero.
 // var numero = prompt("Digite numero");
 // if(numero>0){
 //     alert("Numero mayor a cero");
 // }else{
 //     alert("Numero menor a cero")
 // }
-//444444444444
+//4.	Recibir dos números del usuario, por consola: mostrar la suma, resta, multiplicación y división aplicada entre los dos números ingresados
 // var n1 = parseInt(prompt("Digite numero1"));
 // var n2 = parseInt(prompt("Digite numero2"));
 // sum = n1 + n2;
@@ -13,8 +13,7 @@
 // div = n1 / n2;
 // mul = n1 * n2;
 // alert("Suma: "+ sum + "\n" + "Resta: " + res + "Division: " + div + "Multiplicacion: " + mul)
-//555555555555
-// var fecha = prompt("ingrese su fecha de nacimiento","dd/mm/aaaa");
+//6.	Solicitar al usuario la fecha de nacimiento y a partir de esta información calcular la edad, mostrándola en consola y en un alert
 // var values = fecha.split("/");
 // var dia = values[0];
 // var mes = values[1];
@@ -35,7 +34,7 @@
 // // alert(ahora_ano);
 // // alert(ahora_mes);
 // // alert(ahora_dia);
-//6666666666666666
+//7.	Recibiendo la edad del usuario, mostrar un mensaje diciendo si es menor, mayor de edad o un adulto mayor
 
 // var fecha = prompt("ingrese su fecha de nacimiento","dd/mm/aaaa");
 // var values = fecha.split("/");
@@ -55,10 +54,23 @@
 //     alert("Es mayor de edad("+edad+")");
 // }
 
-//777777777777777
+//8.	Recibiendo el nombre completo del usuario reconocer la cantidad de letras que contiene el nombre, mostrar el resultado por consola.
 // var nombre = prompt("Ingrese su nombre");
 
 // nombre=nombre.split(" ").join("");
 // alert(nombre.length);
 
-//8888888888888
+//9.	Haciendo uso de let generar un error de variable no declarada o inicializada.
+// var a = prompt("digite numero 1")
+// var b = prompt("digite numero 2")
+
+// function suma(){
+//     let sum = a +b;
+// }
+// alert(sum);
+//10.	Haciendo uso de var generar un error de variable no definida.
+// var a;
+// console.log(a);
+//
+
+
