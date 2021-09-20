682d63d6abb4fe7fe2739c7aec45aaa904c45ec3
Julian Rivera <julian.riverah@hotmail.com>
2021-09-19 20:48:51 -0500

    Summary Bootstrap

--- Sprint 4/funciones.js
+++ Sprint 4/funciones.js
@@ -1,6 +1,10 @@
 function imc(){
     var x =parseFloat(document.getElementById('valor1').value);
     var y =parseFloat(document.getElementById('valor2').value);
-    document.getElementById('el-resultado').innerHTML = (y/Math.pow(x,2)).toFixed(2);
     
+    if(isNaN(x) && isNaN(y)){
+        alert("Debe ingresar Talla y Peso");
+    } else{
+        document.getElementById('el-resultado').innerHTML = (y/Math.pow(x,2)).toFixed(2);   
+    }
 }
